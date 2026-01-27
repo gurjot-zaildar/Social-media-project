ca9f30a8aeb3f623bb1099d7c33e1c2801096689
gurjot-zaildar <signingames115@gmail.com>
2026-01-27 15:38:34 +0530

    post controller and auth middleware

---
+++ backend/src/controllers/post.controller.js
@@ -0,0 +1,6 @@
+const postModel = require("../models/post.model")
+
+
+async function postController (req,res){
+    const file = req.file
+}
---
+++ backend/src/routes/post.routes.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router();
+const authMiddleware = require("../middlewares/auth.middleware")
+const postController = require("../controllers/post.controller")
+const multer = require("multer")
+
+const upload = multer({storage: multer.memoryStorage()})
+
+router.post("/",
+    authMiddleware,
+    upload.single("image"),
+    postController)
+
+
+module.exports= router
