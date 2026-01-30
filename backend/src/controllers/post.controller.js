37f9a9ec03d4554532ff17b5741195fd753784a7
gurjot-zaildar <signingames115@gmail.com>
2026-01-30 11:36:07 +0530

    backend compleated

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,5 +1,7 @@
-const postModel = require("../models/post.model")
-const generateCaption = require("../service/ai.service")
+const postModel = require("../models/post.model");
+const generateCaption = require("../service/ai.service");
+const uploadFile = require("../service/storage.service");
+const { randomUUID } = require("crypto");
 
 async function postController (req,res){
     const file = req.file
@@ -8,9 +10,17 @@ async function postController (req,res){
     const base64Image = Buffer.from(file.buffer).toString("base64")
    
     const caption = await generateCaption(base64Image);
+    const result= await uploadFile(file.buffer, `${randomUUID()}`)
 
-   res.json({
-    caption
+    const post = await postModel.create({
+        caption:caption,
+        image: result.url,
+        user:req.user._id
+    })
+
+   res.status(201).json({
+    message:"post created successfully",
+    post
    })
 }
 module.exports={
